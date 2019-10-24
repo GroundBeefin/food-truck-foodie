75c9f7e4084678c19feef9c93892a6b7b50ce21a
Leonela Gutierrez <leonela_gutierrez@hotmail.com>
2019-10-24 10:28:40 -0600

    added files that still need updating

---
+++ user-story.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>User Story</title>
+</head>
+
+<body>
+	<div>
+		<h3>User(Food Trucks):</h3>
+		<p>As a user, I want to share my location with consumers and hosts by posting a message on my profile.</p>
+		<h3>Consumer:</h3>
+		<p>As a consumer, I want to locate a food truck by name or my location.</p>
+		<h3>Host:</h3>
+		<p>As a Host, I want to contact Users for collaboration through contact form.</p>
+	</div>
+</body>
+</html>
