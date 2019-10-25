b81c7e8b66777ba5ad11f37cba67aae64eec3e1d
Ian Foster <ifoster2@cnm.edu>
2019-10-25 10:55:45 -0600

    made ajustment per erd

--- data-design.php
+++ data-design.php
@@ -13,53 +13,52 @@
 
 <h2>Entities & Attributes</h2>
 
-		<h3>User</h3>
 
 <ul>
-	<li>userId</li>
+
+	<h3>User</h3>
+
+	<li>userId(primary key)</li>
 	<li>userActivationToken</li>
 	<li>userAvatarUrl</li>
 	<li>userEmail</li>
 	<li>userHash</li>
-	<li>UserName</li>
+	<li>userName</li>
 </ul>
 
- 		<h3>Truck</h3>
-
 	<ul>
+
+		<h3>Truck</h3>
+
 		<li>truckId</li>
-		<li>truckUserId</li>
-		<li>truckAvatarUrl</li>
+		<li>truckUserId(primary key)</li>
+		<li>truckAvatarUrl(foreign key)</li>
 		<li>truckFoodType</li>
 		<li>truckMenu</li>
 		<li>truckName</li>
+		<li>truckPhoneNumber</li>
 	</ul>
 
+
+
 	<ul>
+
 		<h3>Post</h3>
 
-		<li>postId</li>
-		<li>postTruckId</li>
+		<li>postId(primary key)</li>
+		<li>postTruckId(foreign key)</li>
 		<li>postContent</li>
 		<li>postDatetime</li>
 
 	</ul>
 
-		<h3>Comment</h3>
-<ul>
-
-	<li>commentId(primary key)</li>
-	<li>commentUserId(foreign key)</li>
-	<li>commentContent</li>
-	<li>commentDateTime</li>
-</ul>
 
 		<h3>Relations</h3>
 
 	<p>One <b>User</b> can have many<b>Trucks</b>. (1 - n)<br>
 	One <b>Truck</b> can have one <b>User</b>. (1 - 1)<br>
 	One <b>Truck</b> can make many <b>Posts</b>. (1 - n)<br>
-	One <b>User</b> can make many <b>Comments</b>.(1 - n)
+
 	</p>
 </body>
 </html>
