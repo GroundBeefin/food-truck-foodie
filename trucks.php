8ea5ee499d4e38ff1fd9e0c502b40df36bce0e5a
Ian Foster <ifoster2@cnm.edu>
2019-10-29 16:58:03 -0600

    started new brach with class trucks

---
+++ trucks.php
@@ -0,0 +1,19 @@
+<?php
+namespace Groundbeefin\FoodTruckFoodie;
+require_once ("autoload.php");
+
+use Ramsey\Uuid\Uuid;
+
+/**
+ * This class if the truck user
+ * @package Groundbeefin\FoodTruckFoodie
+ * @author Ian W Foster <ian.foster95@yahoo.com>
+ * @version 001.1
+ */
+class Truck {
+		use ValidateUuid;
+		/**
+		 * This is the id for this profile; this is the primary key
+		 * @var Uuid $truckId
+		 */
+}
