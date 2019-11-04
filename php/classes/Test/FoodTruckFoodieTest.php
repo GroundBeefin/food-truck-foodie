29d7b299c6e5bdc616e939913551032c0aeed853
Leonela Gutierrez <leonela_gutierrez@hotmail.com>
2019-11-04 16:05:10 -0700

    changed namespace and table name in FoodTruckFoodie to fit our project

--- php/classes/Test/FoodTruckFoodieTest.php
+++ php/classes/Test/FoodTruckFoodieTest.php
@@ -42,11 +42,11 @@ abstract class FoodTruckFoodieTest extends TestCase {
 		$dataset = new QueryDataSet($this->getConnection());
 		// add all the tables for the project here
 		// THESE TABLES *MUST* BE LISTED IN THE SAME ORDER THEY WERE CREATED!!!!
-		$dataset->addTable("profile");
-		$dataset->addTable("tweet");
+		$dataset->addTable("user");
+		$dataset->addTable("truck");
 		// the second parameter is required because like is also a SQL keyword and is the only way PHPUnit can query the like table
-		$dataset->addTable("like", "SELECT likeProfileId, likeTweetId, likeDate FROM `like`");
-		$dataset->addTable("image");
+		//$dataset->addTable("like", "SELECT likeProfileId, likeTweetId, likeDate FROM `like`");
+		$dataset->addTable("post");
 		return($dataset);
 	}
 	/**
