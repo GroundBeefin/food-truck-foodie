cbac16771764e934cc973b7237498c6ba7bdda01
Ian Foster <ifoster2@cnm.edu>
2019-11-27 10:42:08 -0700

    added truck and ost to sign up

--- php/public_html/apis/sign-up/index.php
+++ php/public_html/apis/sign-up/index.php
@@ -5,7 +5,7 @@ require_once("/etc/apache2/capstone-mysql/Secrets.php");
 require_once dirname(__DIR__, 3) . "/lib/xsrf.php";
 require_once dirname(__DIR__, 3) . "/lib/uuid.php";
 require_once("/etc/apache2/capstone-mysql/Secrets.php");
-use UssHopper\FoodTruckFoodie\User;
+use UssHopper\FoodTruckFoodie\{User,Truck,Post};
 /**
  * api for signing up too DDC Twitter
  *
