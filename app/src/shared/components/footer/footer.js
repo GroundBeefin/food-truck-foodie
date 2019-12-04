0b0ce394d265954100fbda0e179a28a8ce6cd8c2
Ian Foster <ifoster2@cnm.edu>
2019-12-04 11:58:00 -0700

    added footer

---
+++ app/src/shared/components/footer/footer.js
@@ -0,0 +1,31 @@
+
+import React from "react";
+
+import {Link} from "react-router-dom";
+
+import "../../../index.css";
+
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+
+export const footer = () => (
+	<>
+
+					<footer className="page-footer text-dark py-2 py-md-44">
+							<Container fluid="true">
+									<Row>
+												<Col className="text-center">
+															<FontAwesomeIcon icon={['fab', 'github']} /> &nbsp;
+
+															<a href="https://github.com/GroundBeefin/FoodTruckFoodie" className="text-muted" target="_blank" rel="noopener noreferrer">GitHub</a>
+
+												</Col>
+										</Row>
+								</Container>
+					</footer>
+			</>
+);
