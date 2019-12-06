d4a9ff89410106f15455d65a043f3da75e58a2c6
Zachary Sanchez <Zacharyesanchez22@gmail.com>
2019-12-06 09:23:46 -0700

    edited jumbotron

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -6,17 +6,14 @@ import Jumbotron from "react-bootstrap/Jumbotron";
 export const Home = () => {
 	return (
 		<>
-			<main className="d-flex align-items-center mh-80">
 				<Container fluid="true">
-					<Jumbotron>
-						<h1>Fluid jumbotron</h1>
+					<Jumbotron className="py-10 m-5">
+						<h1>Food Truck Foodie</h1>
 						<p>
-							This is a modified jumbotron that occupies the entire horizontal space of
-							its parent.
+							ABQ's Social Network for Food Trucks
 						</p>
 					</Jumbotron>
 				</Container>
-			</main>
 		</>
 	)
 };
