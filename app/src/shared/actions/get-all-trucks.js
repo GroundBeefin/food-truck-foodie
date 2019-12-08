cd9ab9c68b402e5379546ac26ef80304966d842e
Ian Foster <ifoster2@cnm.edu>
2019-12-08 11:45:46 -0700

    added actions for truck list

---
+++ app/src/shared/actions/get-all-trucks.js
@@ -0,0 +1,6 @@
+import {httpConfig} from "../utils/http-config";
+
+export const getAllTrucks = () => async (dispatch) => {
+	const payload = await httpConfig.get("/apis/truck");
+	dispatch({type: "GET_ALL_TRUCKS",payload : payload.data });
+};
