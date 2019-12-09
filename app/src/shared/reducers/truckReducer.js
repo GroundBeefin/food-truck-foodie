6ffec157461708c127a5412f0f4ddde440832e7a
Ian Foster <ifoster2@cnm.edu>
2019-12-09 14:42:23 -0700

    corrected GroundBeefin in namespace

--- app/src/shared/reducers/truckReducer.js
+++ app/src/shared/reducers/truckReducer.js
@@ -1,5 +1,7 @@
 export default (state = [], action) => {
 	switch(action.type) {
+		case "GET_TRUCK_BY_TRUCK_ID":
+			return action.payload;
 		case "GET_ALL_TRUCKS":
 			return action.payload;
 		default:
