6230f735fbd5c00ed92c261de07bf7569137afc7
Zachary Sanchez <Zacharyesanchez22@gmail.com>
2019-12-09 14:57:56 -0700

    added imports

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -4,6 +4,7 @@ import {getAllTrucks} from "../shared/actions/truck";
 import Card from "react-bootstrap/Card";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Image from "react-bootstrap/Image";
+import MainFTFlogo4 from "../images/MainFTFLogo4.png";
 
 
 export const Home = () => {
