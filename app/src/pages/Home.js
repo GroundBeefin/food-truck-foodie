95302ac7f1fb9898d42eac87a2cf66aeec7ecb5f
Ian Foster <ifoster2@cnm.edu>
2019-12-10 10:36:29 -0700

    updated trucks ttile

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -28,7 +28,7 @@ export const Home = () => {
 					</div>
 				</Jumbotron>
 				<div className="d-flex justify-content-center  my-3 py-3">
-				<h1>LOCAL TRUCKS</h1>
+					<h1>LOCAL TRUCKS</h1>
 				</div>
 			</container-fluid>
 			{trucks.map(truck => {
